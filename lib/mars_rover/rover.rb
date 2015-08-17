39be0e2938e8131e35e6ac52ac0a8c6360eb7163
praveen shukla <praveen@praveens-MacBook-Pro.local>
2015-08-17 13:06:33 +0530

    read me updated.

--- lib/mars_rover/rover.rb
+++ lib/mars_rover/rover.rb
@@ -1,29 +1,29 @@
 module MarsRover
   #rover should able to turn and move
   class Rover
+    TURN_LEFT = {
+      "North" => "West",
+      "West" => "South",
+      "East" => "North",
+      "South" => "East",
+    }
+    TURN_RIGHT = {
+      "North" => "East",
+      "West" => "North",
+      "East" => "South",
+      "South" => "West",
+    }
     def initialize(current_direction, x_coordinate, y_coordinate)
       @current_direction = current_direction
-      @turn_left = {
-        "North" => "West",
-        "West" => "South",
-        "East" => "North",
-        "South" => "East",
-      }
-      @turn_right = {
-        "North" => "East",
-        "West" => "North",
-        "East" => "South",
-        "South" => "West",
-      }
       @move = Move.new(x_coordinate, y_coordinate)
     end
 
     def turn_left
-      @turn_left[@current_direction]
+      TURN_LEFT[@current_direction]
     end
 
     def turn_right
-      @turn_right[@current_direction]
+      TURN_RIGHT[@current_direction]
     end
 
     def move_north
